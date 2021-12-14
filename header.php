438625b37312ea9df040eff0f5bc4ac452ce38fb
rodrigo <rodryest34@hotmail.com>
2021-12-14 00:49:48 -0600

    Version Final del CRUD

---
+++ header.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <!--<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">-->
+    <!--<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>-->
+    <link rel="stylesheet" href="librerias/css/B4/bootstrap.min.css">
+    <link rel="stylesheet" href="librerias/css/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="librerias/datatables/bootstrap.css">
+    <link rel="stylesheet" href="librerias/datatables/dataTables.bootstrap4.min.css">
+    <link rel="shortcut icon" href="librerias/icon/Xanna.ico">
+    <title>CRUD MongoDB</title>
+</head>
+
+<body>
